26bcf5cf169b709b68333045949c43adbe496513
Akhtarnaaz <01fe24ica054@kletech.ac.in>
2025-10-30 20:09:18 +0530

    Added new Functionalities to the program (Multiplication & Division)

--- Operations.py
+++ Operations.py
@@ -7,7 +7,6 @@ n2 = float(input("Enter second number: "))
 # Performing addition and subtraction
 sum_result = n1 + n2
 diff_result = n1 - n2
-
 # Displaying the results
 print("\nResults:")
 print(f"Addition: {n1} + {n2} = {sum_result}")
